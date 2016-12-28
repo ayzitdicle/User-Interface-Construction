7b136301699f0b84a43bc21ef4443a6192df9c60
dicle <e1819051@ceng.metu.edu.tr>
2016-12-29 00:40:59 +0300

    Responsive UI of Time Banking System

--- responsive/Main_Page.js
+++ responsive/Main_Page.js
@@ -7,3 +7,18 @@
  	var selText = $(this).text();
   	$(this).parents('.btn-group').find('.dropdown-toggle').html(selText);
 });
+
+
+
+
+function sum() {
+  var sum = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  console.log(sum);
+}
+
+function send_offer(){
+	console.log(sum(1,2,3,4,5));
+}
